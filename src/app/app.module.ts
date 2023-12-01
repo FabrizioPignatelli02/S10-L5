f5b88c378524f0f37b3e67d1106ed05bdb53244c
Fabrizio Giuseppe Pignatelli <141920930+FabrizioPignatelli02@users.noreply.github.com>
2023-12-01 10:53:46 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Route } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { CompletedComponent } from './components/completed/completed.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+const routes: Route[] = [
+  {
+    path: "",
+    component: TodoComponent
+  },
+  {
+    path: "completed",
+    component: CompletedComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    CompletedComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/items.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Item } from '../models/item';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemsService {
+
+  constructor() { }
+}
